1a394054dc29d8f9539ac407481e464f94b6ea6d
anraku <anraku@localhost.localdomain>
2017-05-14 16:02:23 +0900

    commit

---
+++ test/Assets/Script/BallScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var Speed : float = 15.0;
+
+/*
+function Start () {
+	
+}
+*/
+
+function Update () {
+    if (Input.GetButtonUp("Jump") && GetComponent.<Rigidbody>().velocity == Vector3(0, 0, 0)){
+        GetComponent.<Rigidbody>().AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
+    }
+}
+
+function OnCollisionEnter () {
+    GetComponent.<Rigidbody>().velocity = GetComponent.<Rigidbody>().velocity.normalized * 15;
+ 
+    if (Mathf.Abs(GetComponent.<Rigidbody>().velocity.y) < 5) {
+        GetComponent.<Rigidbody>().velocity.y *= 5;
+    }
+    if (Mathf.Abs(GetComponent.<Rigidbody>().velocity.x) < 5) {
+        GetComponent.<Rigidbody>().velocity.x *= 5;
+    }
+ 
+}
---
+++ test/Assets/Script/BlockScript.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var obj :GameObject;
+var script :SceneScript;
+ 
+obj = GameObject.Find("SceneScript");
+script = obj.GetComponent(SceneScript);
+
+function OnCollisionEnter(){
+	Destroy(gameObject);
+	script.score += 10;
+	script.blockCt--;
+}
